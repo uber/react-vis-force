0285cd6e33bbf942494081654866e85e0be9f0ef
Paul Marbach <pmarbach@uber.com>
2016-10-04 13:07:01 -0400

    finish up demo

--- src/demo/index.js
+++ src/demo/index.js
@@ -18,15 +18,42 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-import React from 'react';
+import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import { document } from 'global';
+import { PrismCode } from 'react-prism';
 
 import './Demo.css';
 import SimpleExample from './SimpleExample';
 import ZoomableExample from './ZoomableExample';
 import InteractiveExample from './InteractiveExample';
 import LesMisExample from './LesMisExample';
+
+const DemoExample = ({ children, codeSample, title }) => (
+  <article>
+    <h2>{title}</h2>
+    <div className="ui two column grid">
+      <div className="column">
+        <h4>Demo</h4>
+        {children}
+      </div>
+      <div className="column">
+        <h4>Code</h4>
+        <pre className="ui segment">
+          <PrismCode className="language-javascript language-jsx">
+            {codeSample}
+          </PrismCode>
+        </pre>
+      </div>
+    </div>
+  </article>
+);
+
+DemoExample.propTypes = {
+  children: PropTypes.any,
+  codeSample: PropTypes.string,
+  title: PropTypes.any,
+};
 
 ReactDOM.render(
   <section style={{ padding: '1em' }}>
@@ -46,22 +73,87 @@ ReactDOM.render(
 
     <h1>{'react-vis-force demo'}</h1>
 
-    <h2>{'Simple example'}</h2>
-    <SimpleExample />
+    <DemoExample
+      title="Simple Example"
+      codeSample={`<ForceGraph simulationOptions={{ height: 300, width: 300 }}>
+  <ForceGraphNode node={{ id: 'first-node', radius: 5 }} fill="red" />
+  <ForceGraphNode showLabel node={{ id: 'second-node', radius: 5 }} fill="blue" />
+  <ForceGraphLink link={{ source: 'first-node', target: 'second-node' }} />
+</ForceGraph>`}
+    >
+      <SimpleExample />
+    </DemoExample>
 
-    <h2>{'Zoomable example'}</h2>
-    <ZoomableExample />
+    <DemoExample
+      title="Zoomable Example"
+      codeSample={`<ForceGraph zoom minScale={1} maxScale={3} simulationOptions={{ height: 300, width: 300 }}>
+  <ForceGraphNode node={{ id: 'first-node', radius: 5 }} fill="red" />
+  <ForceGraphNode node={{ id: 'second-node', radius: 5 }} fill="blue" />
+  <ForceGraphLink link={{ source: 'first-node', target: 'second-node' }} />
+  <line x1={150} y1={-1000} x2={150} y2={1300} zoomable stroke="green" />
+  <text x={100} y={100}>{'not zoomed'}</text>
+</ForceGraph>`}
+    >
+      <ZoomableExample />
+    </DemoExample>
 
-    <h2>{'Interactive example'}</h2>
-    <InteractiveExample />
+    <DemoExample
+      title="Interactive Example"
+      codeSample={`<InteractiveForceGraph
+  simulationOptions={{ height: 300, width: 300 }}
+  labelAttr="label"
+  onSelectNode={(event, node) => console.log(node)}
+>
+  <ForceGraphNode node={{ id: 'first-node', label: 'First node', radius: 5 }} fill="red" />
+  <ForceGraphNode node={{ id: 'second-node', label: 'Second node', radius: 5 }} fill="blue" />
+  <ForceGraphLink link={{ source: 'first-node', target: 'second-node' }} />
+</InteractiveForceGraph>`}
+    >
+      <InteractiveExample />
+    </DemoExample>
 
-    <h2>
-      {'Les Mis demo, adapted from '}
-      <a href="https://bl.ocks.org/mbostock/4062045">
-        {'Mike Bostock\'s d3-force demo.'}
-      </a>
-    </h2>
-    <LesMisExample />
+    <DemoExample
+      title={(
+        <span>
+          {'Les Mis demo, adapted from '}
+          <a href="https://bl.ocks.org/mbostock/4062045">
+            {'Mike Bostock\'s d3-force demo.'}
+          </a>
+        </span>
+      )}
+      codeSample={`<InteractiveForceGraph
+  simulationOptions={{
+    animate: true,
+    strength: {
+      charge: node => -15.5 * node.radius,
+      collide: () => 0.75,
+    },
+  }}
+  zoom
+  highlightDependencies
+  minScale={(1 / 2)}
+  maxScale={4}
+>
+  {lesMisJSON.nodes.map(node => (
+    <ForceGraphNode
+      key={node.id}
+      fill={scale(node.group)}
+      node={{
+        ...node,
+        radius: Math.floor(Math.sqrt(radii[node.id])) * 2,
+      }}
+    />
+  ))}
+  {lesMisJSON.links.map(link => (
+    <ForceGraphLink
+      key={\`\${link.source}=>\${link.target}\`}
+      link={link}
+    />
+  ))}
+</InteractiveForceGraph>`}
+    >
+      <LesMisExample />
+    </DemoExample>
   </section>,
   document.getElementById('root')
 );
