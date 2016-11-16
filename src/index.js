4bb733e85b2ee76eba4ca27beb5a9bcad41ae107
Aleš Roubíček <ales@roubicek.name>
2016-11-16 07:40:15 +0100

    Added path based node with correct length for arrows

--- src/index.js
+++ src/index.js
@@ -21,5 +21,6 @@
 export { createSimulation, updateSimulation } from './utils/d3-force';
 export { default as ForceGraph } from './components/ForceGraph';
 export { default as ForceGraphLink } from './components/ForceGraphLink';
+export { default as ForceGraphArrowLink } from './components/ForceGraphArrowLink';
 export { default as ForceGraphNode } from './components/ForceGraphNode';
 export { default as InteractiveForceGraph } from './components/InteractiveForceGraph';
